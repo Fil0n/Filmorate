7c0b156ef5381f4f80a3d9f2d6fec57c044f8e49
shishkin <alexconermail@gmail.com>
2025-03-05 20:24:33 +0300

    Функциональность «Рекомендации»

---
+++ src/main/java/ru/yandex/practicum/filmrate/helper/BinarySlopeOne.java
@@ -0,0 +1,65 @@
+package ru.yandex.practicum.filmrate.helper;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class BinarySlopeOne {
+    private Map<Long, Map<Long, Double>> matrix = new HashMap<>();
+    private Map<Long, Set<Long>> inputData;
+
+    public BinarySlopeOne(Map<Long, Set<Long>> inputData) {
+        this.inputData = inputData;
+        buildDifferenceMatrix();
+    }
+
+    private void buildDifferenceMatrix() {
+        for (Long product1 : inputData.keySet()) {
+            matrix.putIfAbsent(product1, new HashMap<>());
+
+            for (Long product2 : inputData.keySet()) {
+                if (product1.equals(product2)) continue;
+
+                Set<Long> scoreSet1 = inputData.get(product1);
+                Set<Long> scoreSet2 = inputData.get(product2);
+
+                int occurences = 0;
+                for (Long userId : scoreSet1) {
+                    if (scoreSet2.contains(userId)) {
+                        occurences++;
+                    }
+                }
+
+                double diff = (double) occurences / scoreSet1.size();
+                matrix.get(product1).put(product2, diff);
+            }
+        }
+    }
+
+    public Map<Long, Double> predict(Long userid) {
+        Set<Long> productIds = new HashSet<>();
+        // получаем все фильмы, которые оценил пользователь
+        for (Map.Entry<Long, Set<Long>> entry : inputData.entrySet()) {
+            if (entry.getValue().contains(userid)) {
+                productIds.add(entry.getKey());
+            }
+        }
+
+        Map<Long, Double> predictions = new HashMap<>();
+
+        for (Long productId : productIds) {
+            if (matrix.containsKey(productId)) {
+                for (Map.Entry<Long, Double> entry : matrix.get(productId).entrySet()) {
+                    Long recommendedProductId = entry.getKey();
+                    double diff = entry.getValue();
+
+                    if (!productIds.contains(recommendedProductId)) {
+                        predictions.put(recommendedProductId, predictions.getOrDefault(recommendedProductId, 0.0) + diff);
+                    }
+                }
+            }
+        }
+        return predictions;
+    }
+}
